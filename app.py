e288fcf3c49ae11268269dc8b1559fc93132b5b4
devdsml <95732612+devdsml@users.noreply.github.com>
2022-03-04 16:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,28 +11,18 @@ USERNAME_PASSWORD_PAIRS = [
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
- 
- 
- 
 app.layout = html.Div([
-    dcc.RangeSlider(
-        id='range-slider',
-        min=-5,
-        max=6,
-        marks={i:str(i) for i in range(-5, 7)},
-        value=[-3, 4]
-    )]),
-    html.H1(id='product')  # this is the output,
- html.Div([dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
+    dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
     html.Div(id='dd-output-container')
-], style={'width':'50%'})
- 
+])
+
+
 @app.callback(
-     Output('dd-output-container', 'children'),
-    Input('demo-dropdown', 'value'))
+    Output('dd-output-container', 'children'),
+    Input('demo-dropdown', 'value')
+)
 def update_output(value):
     return f'You have selected {value}'
-    
  
 if __name__ == '__main__':
     app.run_server(debug=True)
