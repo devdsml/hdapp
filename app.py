4795815d9f07fa125bcc94a0e32c4eb91c45b3cc
devdsml <95732612+devdsml@users.noreply.github.com>
2022-03-04 16:39:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,33 @@ import dash
 import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 from dash.dependencies import Input, Output
  
 USERNAME_PASSWORD_PAIRS = [
     ['nethu', '12345'],['guvi', 'guvi'],['tstn','testn']
 ]
- 
+df = pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/matches.csv')
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
 app.layout = html.Div([
-    dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
-    html.Div(id='dd-output-container')
+    dcc.Dropdown(['Mumbai Indians', 'MTL', 'SF'], 'NYC', id='baseddown'),
+    html.Div(dcc.Graph(
+       id='graph-1',
+      ))
 ])
 
 
 @app.callback(
-    Output('dd-output-container', 'children'),
-    Input('demo-dropdown', 'value')
+    Output('graph-1', 'children'),
+    Input('baseddown', 'value')
 )
-def update_output(value):
-    return f'You have selected {value}'
+def update_graph(value):
+      df1 = df[df['winner'] ==value]
+      fig5=px.bar(df1,x='venue',title='Luckiest Venue')
+      fig5.update_traces(textinfo="label+value",textposition='inside')
+      return fig5
  
 if __name__ == '__main__':
     app.run_server(debug=True)
